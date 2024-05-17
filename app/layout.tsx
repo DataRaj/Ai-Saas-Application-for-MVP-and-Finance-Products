36f54306590df6aa249d57453edf1769c9b8dba2
dataraj <dhadas808@yahoo.com>
2024-05-17 21:36:10 +0530

    overall changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import DashboardLayout from "./(dashboard)/layout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      
     </html>
     </ClerkProvider>
   );
