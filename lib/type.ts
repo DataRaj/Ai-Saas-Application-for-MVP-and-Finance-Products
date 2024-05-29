13106bbe04c90e89a2a956ba894720bf8f80fe22
dataraj <dhadas808@yahoo.com>
2024-05-29 23:55:36 +0530

    components/pages/workflows/workflow-button.tsx
    overall modified the things today

--- lib/type.ts
+++ lib/type.ts
@@ -1,7 +1,56 @@
-import { z } from "zod";
+import { ConnectionProviderProps } from "@/provider/connection-provider";
+import { number, z } from "zod";
 
 export const EditUserProfileSchema = z.object({
     email: z.string().email('Required'),
     name: z.string().min(1,'Required')
 })
 
+export const WorkFlowFormSchema = z.object({
+    name: z.string().min(1,'required'),
+    description: z.string().min(1,'required'),
+})
+
+type ConnectionTypes = "Google Drive" | "Discord" | "Slack" | "Notion"
+
+export type Connection ={
+    title:ConnectionTypes
+    description: string
+    image: string
+    connectionKey: keyof ConnectionProviderProps
+    accessKeyToken?: string
+    alwaysTrue?: boolean
+    slackSpecial?: boolean
+}
+
+export type EditorCanvasType = 
+    | 'Email'
+    | 'Condition'
+    | 'AI'
+    | 'Slack'
+    | 'Google Drive'
+    | 'Notion'
+    | 'Custom Webhook'
+    | 'Google Calender'
+    | 'Trigger'
+    | 'Action'
+    | 'Wait';
+
+export type EditorCanvasCardType ={
+    title: string
+    description: string
+    completed:boolean
+    current: boolean
+    metadata : any
+    type: EditorCanvasType
+}
+
+export type editorNodeType={
+    id: string
+    type: EditorCanvasCardType['type']
+    position: {
+        x: number,
+        y: number
+    }
+    data: EditorCanvasCardType;
+} 
