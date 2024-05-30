396bdf381382dd724f21347774f35c12bf633b4a
dataraj <dhadas808@yahoo.com>
2024-05-30 23:59:32 +0530

    added files

--- provider/connection-provider.tsx
+++ provider/connection-provider.tsx
@@ -1,13 +1,103 @@
 'use client'
 
-import React from 'react'
-export type ConnectionProviderProps ={}
-const ConnectionProvider = (props: ConnectionProviderProps) => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { Dispatch, SetStateAction, createContext, useContext } from 'react'
+export type ConnectionProviderProps ={
+  discordNode: {
+    webhookURL: string,
+    content: string,
+    webhookName: string,
+    buildName: string
+  }
+  setDiscordNode: Dispatch<SetStateAction<any>>
+  googleNode: {}[]
+
+  setGoogleNode: Dispatch<SetStateAction<any>>
+  notionNode : {
+    accessToken: string
+     databaseId: string
+     workspaceName: string
+     content: string
+    //  {
+    //   name:string
+    //   kind:string
+    //   type:string
+
+    //  }
+  }
+  workflowTemplate: {
+    discord?: string
+    notion?:string
+    slack?:string
+  }
+
+  setNotionNode: Dispatch<SetStateAction<any>>
+  slackNode:{
+    appId: string
+    authedUserId:string
+    authedUserToken:string
+    slackAccessToken:String
+    botUserId: string
+    teamId: string
+    teamName:string
+    content:string
+  }
+  setSlackNode : Dispatch<SetStateAction<any>>
+  setWorkFlowTemplate: Dispatch<SetStateAction<{
+    discord?:string
+    notion?:string
+    slack?:string
+  }>>
+  isLoading:boolean
+  setIsLoading: Dispatch<SetStateAction<boolean>>
+}
+const InitialValues: ConnectionProviderProps = {
+  discordNode: {
+    webhookURL: '',
+    content: '',
+    webhookName: '',
+    buildName: '',
+  },
+  googleNode: [],
+  notionNode: {
+    accessToken: '',
+    databaseId: '',
+    workspaceName: '',
+    content: '',
+  },
+  workflowTemplate: {
+    discord: '',
+    notion: '',
+    slack: '',
+  },
+  slackNode: {
+    appId: '',
+    authedUserId: '',
+    authedUserToken: '',
+    slackAccessToken: '',
+    botUserId: '',
+    teamId: '',
+    teamName: '',
+    content: '',
+  },
+  isLoading: false,
+  setGoogleNode: () => undefined,
+  setDiscordNode: () => undefined,
+  setNotionNode: () => undefined,
+  setSlackNode: () => undefined,
+  setIsLoading: () => undefined,
+  setWorkFlowTemplate: () => undefined,
 }
 
-export default ConnectionProvider
+const ConnectionProvider = createContext(InitialValues);
+const {provider} = ConnectionProvider
+
+
+// const ConnectionProvider = (props: ConnectionProviderProps) => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ConnectionProvider
