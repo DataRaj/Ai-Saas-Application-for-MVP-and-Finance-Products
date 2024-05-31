3c793819a9b3d9074f0a1f72089c9e58849c8be1
dataraj <dhadas808@yahoo.com>
2024-05-31 23:30:08 +0530

    added more files

--- lib/type.ts
+++ lib/type.ts
@@ -1,56 +1,92 @@
-import { ConnectionProviderProps } from "@/provider/connection-provider";
-import { number, z } from "zod";
+import { ConnectionProviderProps } from '@/provider/connection-provider'
+import { z } from 'zod'
 
 export const EditUserProfileSchema = z.object({
-    email: z.string().email('Required'),
-    name: z.string().min(1,'Required')
+  email: z.string().email('Required'),
+  name: z.string().min(1, 'Required'),
 })
 
-export const WorkFlowFormSchema = z.object({
-    name: z.string().min(1,'required'),
-    description: z.string().min(1,'required'),
+export const WorkflowFormSchema = z.object({
+  name: z.string().min(1, 'Required'),
+  description: z.string().min(1, 'Required'),
 })
 
-type ConnectionTypes = "Google Drive" | "Discord" | "Slack" | "Notion"
+export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord'
 
-export type Connection ={
-    title:ConnectionTypes
-    description: string
-    image: string
-    connectionKey: keyof ConnectionProviderProps
-    accessKeyToken?: string
-    alwaysTrue?: boolean
-    slackSpecial?: boolean
+export type Connection = {
+  title: ConnectionTypes
+  description: string
+  image: string
+  connectionKey: keyof ConnectionProviderProps
+  accessTokenKey?: string
+  alwaysTrue?: boolean
+  slackSpecial?: boolean
 }
 
-export type EditorCanvasType = 
-    | 'Email'
-    | 'Condition'
-    | 'AI'
-    | 'Slack'
-    | 'Google Drive'
-    | 'Notion'
-    | 'Custom Webhook'
-    | 'Google Calender'
-    | 'Trigger'
-    | 'Action'
-    | 'Wait';
+export type EditorCanvasTypes =
+  | 'Email'
+  | 'Condition'
+  | 'AI'
+  | 'Slack'
+  | 'Google Drive'
+  | 'Notion'
+  | 'Custom Webhook'
+  | 'Google Calendar'
+  | 'Trigger'
+  | 'Action'
+  | 'Wait'
 
-export type EditorCanvasCardType ={
-    title: string
-    description: string
-    completed:boolean
-    current: boolean
-    metadata : any
-    type: EditorCanvasType
+export type EditorCanvasCardType = {
+  title: string
+  description: string
+  completed: boolean
+  current: boolean
+  metadata: any
+  type: EditorCanvasTypes
 }
 
-export type editorNodeType={
-    id: string
-    type: EditorCanvasCardType['type']
-    position: {
-        x: number,
-        y: number
+export type EditorNodeType = {
+  id: string
+  type: EditorCanvasCardType['type']
+  position: {
+    x: number
+    y: number
+  }
+  data: EditorCanvasCardType
+}
+
+export type EditorNode = EditorNodeType
+
+export type EditorActions =
+  | {
+      type: 'LOAD_DATA'
+      payload: {
+        elements: EditorNode[]
+        edges: {
+          id: string
+          source: string
+          target: string
+        }[]
+      }
     }
-    data: EditorCanvasCardType;
-} 
+  | {
+      type: 'UPDATE_NODE'
+      payload: {
+        elements: EditorNode[]
+      }
+    }
+  | { type: 'REDO' }
+  | { type: 'UNDO' }
+  | {
+      type: 'SELECTED_ELEMENT'
+      payload: {
+        element: EditorNode
+      }
+    }
+
+export const nodeMapper: Record<string, string> = {
+  Notion: 'notionNode',
+  Slack: 'slackNode',
+  Discord: 'discordNode',
+  'Google Drive': 'googleNode',
+}
