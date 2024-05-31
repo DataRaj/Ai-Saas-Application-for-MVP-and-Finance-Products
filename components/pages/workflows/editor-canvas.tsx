d359d8550b1de47d8ebe2d297bfa06cdd3bafe19
dataraj <dhadas808@yahoo.com>
2024-05-31 23:29:50 +0530

    added editor canvas and sonner

---
+++ components/pages/workflows/editor-canvas.tsx
@@ -0,0 +1,16 @@
+import { editorNodeType } from "@/lib/type";
+import { EditorProvider} from "@/provider/editor-provider";
+
+
+import React from 'react'
+type props = {
+}
+const EditorCanvas = (props: props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditorCanvas
