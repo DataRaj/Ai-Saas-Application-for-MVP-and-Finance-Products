4263868ce968a6cf433bc61e667ff2efc3ff45de
dataraj <dhadas808@yahoo.com>
2024-06-03 02:11:33 +0530

    changed from component location of main and vav dynamic pages

---
+++ app/(nav)/(pages)/workflows/editor/[editorId]/_components/render-output-accordian.tsx
@@ -0,0 +1,31 @@
+import { ConnectionProviderProps } from '@/provider/connection-provider'
+import { EditorState } from '@/provider/editor-provider'
+import { useFuzzieStore } from '@/store'
+import React from 'react'
+import ContentBasedOnTitle from './content-based-on-title'
+
+type Props = {
+  state: EditorState
+  nodeConnection: ConnectionProviderProps
+}
+
+const RenderOutputAccordion = ({ state, nodeConnection }: Props) => {
+  const {
+    googleFile,
+    setGoogleFile,
+    selectedSlackChannels,
+    setSelectedSlackChannels,
+  } = useFuzzieStore()
+  return (
+    <ContentBasedOnTitle
+      nodeConnection={nodeConnection}
+      newState={state}
+      file={googleFile}
+      setFile={setGoogleFile}
+      selectedSlackChannels={selectedSlackChannels}
+      setSelectedSlackChannels={setSelectedSlackChannels}
+    />
+  )
+}
+
+export default RenderOutputAccordion
